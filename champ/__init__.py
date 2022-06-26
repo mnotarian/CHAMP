3bfa2bd719c0216daae790c700dff2adc38b924d
Matthew Notarian <97697143+mnotarian@users.noreply.github.com>
2022-06-26 14:29:06 -0400

    add parallel_leiden_CPM to _init_.py

--- champ/__init__.py
+++ champ/__init__.py
@@ -26,6 +26,7 @@ from .louvain_ext import adjacency_to_edges
 
 from .leiden_ext import run_leiden
 from .leiden_ext import parallel_leiden
+from .leiden_ext import parallel_leiden_CPM
 
 from .PartitionEnsemble import PartitionEnsemble
 
