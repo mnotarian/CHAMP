aa3d8ce3ed226003fcb850484cd2f8a4455d6a30
Matthew Notarian <97697143+mnotarian@users.noreply.github.com>
2022-06-27 09:03:49 -0400

    Added custom plot functions
    restrict plots to input gamma range

--- champ/plot_domains.py
+++ champ/plot_domains.py
@@ -165,6 +165,55 @@ def plot_single_layer_modularity_domains(ind_2_domains, ax=None, colors=None, la
     return ax
 
 
+def plot_single_layer_modularity_domains_custom(ind_2_domains, res, mods, ax=None, colors=None, labels=None):
+    '''
+    Plot the piece-wise linear curve for CHAMP of single layer partitions
+    :param ind_2_domains: dictionary mapping partition index to domain of dominance
+    :type ind_2_domains: { ind: [ np.array(gam_0x, gam_0y), np.array(gam_1x, gam_1y)  ] ,...}
+    :param res: list of gamma resolutions 
+    :para mods: list of original modularities
+    :param ax: Matplotlib Axes object to draw the graph on
+    :param colors: Either a single color or list of colors with same length as number of domains
+    :return: ax  Reference to the ax on which plot is drawn.
+    '''
+    allgams = [res[ind] for ind in rand_ind]
+    allmods=[mods[ind] for ind in rand_ind]
+    
+    if ax==None:
+        f=plt.figure()
+        ax=f.add_subplot(111)
+
+    if colors==None:
+        cnorm=mcolors.Normalize(vmin=0,vmax=len(ind_2_domains))
+        cmap=cm.get_cmap("Set1")
+        pal=lmap(lambda i : cmap(cnorm(i)),range(len(ind_2_domains)))
+    i=0
+
+    for i,ind_pts in enumerate(iteritems(ind_2_domains)):
+        ind, pts = ind_pts
+        if hasattr(colors, "__iter__"):
+            assert len(colors) == len(ind_2_domains)
+            c=colors[i] #must match length
+        else:
+            c=pal[i] if colors == None else colors
+        coords=lmap(list,zip(*pts))
+
+
+        ax.scatter(coords[0],coords[1],color=c,marker='x')
+        ax.plot(coords[0],coords[1],color=c,lw=2,alpha=.75)
+        ax.set_ylim([np.min(allmods) - 100, np.max(allmods) + 100])
+        ax.set_xlim(xmin=0, xmax=max(allgams))
+
+    if labels is not None:
+        if labels is True:
+            ax.set_ylabel("modularity")
+            ax.set_xlabel("resolution")
+        else:
+            ax.set_xlabel(labels[0])
+            ax.set_ylabel(labels[1])
+
+    return ax
+
 
 def plot_similarity_heatmap_single_layer(partitions, index_2_domain, partitions_other=None,index_2_dom_other=None,
                                          sim_mat=None,ax=None, cmap=None, title=None):
@@ -186,7 +235,7 @@ def plot_similarity_heatmap_single_layer(partitions, index_2_domain, partitions_
     :rtype: matplolib.Axes,np.array
 
     '''
-
+    
     if cmap is None:
         cmap = 'PuBu'
 
@@ -242,6 +291,93 @@ def plot_similarity_heatmap_single_layer(partitions, index_2_domain, partitions_
 
     ax.set_ylim(gamma_transitions[0], max(gamma_transitions))
     ax.set_xlim(gamma_transitions2[0], max(gamma_transitions2))
+    if not title is None:
+        if title is True:
+            ax.set_title('Adjusted Mutual Information between Partitions')
+        else:
+            ax.set_title(title)
+    plt.colorbar(pmap, ax=ax)
+    return ax, AMI_mat
+
+def plot_similarity_heatmap_single_layer_custom(partitions, index_2_domain, res, partitions_other=None,index_2_dom_other=None,
+                                         sim_mat=None,ax=None, cmap=None, title=None):
+    '''
+
+    :param partitions:
+    :type partitions:
+    :param index_2_domain:
+    :type index_2_domain:
+    :param res:
+    :param sim_mat:
+    :type sim_mat:
+    :param ax: Axes to draw the figure on.  New figure created if not supplied.
+    :type ax: Matplotlib.Axes
+    :param cmap: Color mapping.  Default is plasma
+    :type cmap: Matplotlib.colors.Colormap
+    :param title: True if add generic title, or string of title to add
+    :type title: boolean or string
+    :return: axis drawn on , computed similarity matrix
+    :rtype: matplolib.Axes,np.array
+
+    '''
+
+    allgams = [res[ind] for ind in rand_ind]
+    
+    if cmap is None:
+        cmap = 'PuBu'
+
+    if ax is None:
+        f=plt.figure()
+        ax=f.add_subplot(111)
+
+    ind_vals=list(zip(index_2_domain.keys(),[val[0][0] for val in index_2_domain.values()]))
+    ind_vals.sort(key=lambda x:x[1])
+    # Take the x coordinate of first point in each domain
+    gamma_transitions = [val for ind, val in ind_vals]
+    gamma_transitions.append(index_2_domain[ind_vals[-1][0]][-1][0])  # Append last point of last partition
+
+    #if there isn't a partition set to compare to the default is to compare to self.
+    if not( index_2_dom_other is None or partitions_other is None):
+        ind_vals2 = zip(index_2_dom_other.keys(), [val[0][0] for val in index_2_dom_other.values()])
+        ind_vals2.sort(key=lambda x: x[1])
+        gamma_transitions2 = [val for ind, val in ind_vals2]
+        gamma_transitions2.append(index_2_dom_other[ind_vals2[-1][0]][-1][0])
+    else:
+        ind_vals2 = ind_vals
+        gamma_transitions2=gamma_transitions
+        partitions_other=partitions
+
+
+    # G2S, G1S = np.meshgrid(gamma_transitions2, gamma_transitions)
+    G1S, G2S = np.meshgrid(gamma_transitions2, gamma_transitions)
+    if sim_mat is None:
+        AMI_mat = np.zeros((len(ind_vals), len(ind_vals2)))
+        for i  in range(len(ind_vals)):
+            for j in range(len(ind_vals2)):
+                partition1=partitions[ind_vals[i][0]]
+                partition2=partitions_other[ind_vals2[j][0]]
+
+                AMI_mat[i][j] = adjusted_mutual_info_score(partition1,
+                                                            partition2,average_method='max')
+                # AMI_mat[j][i] = AMI_mat[i][j] #symmetric
+    else:
+        AMI_mat = sim_mat
+
+
+    # pal = sbn.cubehelix_palette(start=2, rot=.3, dark=0, light=.95, reverse=True, as_cmap=True)
+
+
+    pmap = ax.pcolor(G1S, G2S, AMI_mat, cmap=cmap)
+    ax.set_xlabel("resolution")
+    ax.set_ylabel("resolution")
+
+    for gm in gamma_transitions:
+        ax.axhline(gm, color='k', linestyle='dashed')
+    for gm in gamma_transitions2:
+        ax.axvline(gm, color='k', linestyle='dashed')
+
+    ax.set_ylim(gamma_transitions[0], max(allgams))
+    ax.set_xlim(gamma_transitions2[0], max(allgams))
     if not title is None:
         if title is True:
             ax.set_title('Adjusted Mutual Information between Partitions')
